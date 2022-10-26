384b74b809d2a3b31c0e32a88623c2301785caeb
Andres Matarrita <jose.matarritamiranda>
2022-10-26 14:50:51 -0600

    TableroAbstracto.h

---
+++ TableroAbstracto.h
@@ -0,0 +1,41 @@
+#ifndef MI_TABLEROABSTRACTO
+#define MI_TABLEROABSTRACTO
+using namespace std;
+
+
+class TableroAbstracto
+{
+public:
+	TableroAbstracto() {};
+	~TableroAbstracto() {
+        //delete jugadores
+        //delete dado
+    };
+    
+
+	virtual void asignarJugadores(int cantidadJugadores) = 0; 
+	virtual void ubicarJugadores()= 0; // Fichas enel vector zona segurado
+	virtual void iniciarPartida(){
+    
+    // dado = new dado()
+
+    }
+    virtual void pasarTurno() = 0; //Cambia al jugador actual
+    virtual void jugarTurno() = 0;      // Tira dado y ubica las fichas
+	void obtenerJugador(/*color jugador*/){}
+	void obtenerJugadorActual(){}
+    void asignarJugadorActual(/*jugador actual*/){}
+	void obtenerJugadores(){}
+    
+
+	private:
+	// vector  *jugadores
+	// jugador actual
+    // Dado * dado
+    int juegoTerminado;
+};
+
+#endif
+
+
+
