20655c07164fd466ddf47b8ae1465d1568342257
JosephnunezS <70175467+JosephnunezS@users.noreply.github.com>
2022-10-30 20:58:59 -0600

    Implementacion de regla y metodo IniciarReglas en ValidadorLudo.

--- ValidadorLudo.cpp
+++ ValidadorLudo.cpp
@@ -31,6 +31,18 @@ ValidadorLudo::ValidadorLudo(TableroLudo * mesa) {
  * 
  */
 void ValidadorLudo::iniciarReglas() {
+  if (!this->reglas.size()) {
+    this->reglas.push_back(new Regla("1. Al comenzar, todo jugador tiene 4 fichas del mismo color en la salida."));
+    this->reglas.push_back(new Regla("2. Para determinar cual jugador inicia, la máquina lanza un número al azar de 1 hasta\nel número de jugadores, el número resultante correspondera al jugador que inicia el\njuego."));
+    this->reglas.push_back(new Regla("3. Para sacar una ficha de la salida, se necesita que el jugador obtenga 6 al lanzar\nel dado. Si el primer jugador no obtiene un 6, entonces inicia el turno de los demás\njugadores, yendo en dirección de las manecillas del reloj."));
+    this->reglas.push_back(new Regla("4. Una vez que el jugador tenga por lo menos una ficha por fuera de la salida, tiene la opción de lanzar el dado nuevamente. El número que caiga corresponderá a la cantidad de casillas que puede avanzar en ese turno."));
+    this->reglas.push_back(new Regla("5. Las fichas avanzan en la dirección que indíque la primera ficha del tablero."));
+    this->reglas.push_back(new Regla("6. Si un jugador vuelve a sacar 6 al lanzar el dado, entonces tiene la opción de sacar\notra ficha de la salida. Si el jugador lo prefiere, también puede hacer que alguna\notra ficha en el tablero que ya esté afuera de la salida se mueva 6 posiciones."));
+    this->reglas.push_back(new Regla("7. En cualquier caso que se de, si el jugador obtiene 6 al lanzar el dado, podrá\nlanzarlo de nuevo consecutivamente."));
+    this->reglas.push_back(new Regla("8. Si dos fichas, independientemente de su color, se encuentran en una misma ficha,\nesta forma una barrera por la cual ninguna otra ficha puede pasar."));
+    this->reglas.push_back(new Regla("9. Si una ficha cae en una casilla en la que se encuentra otra ficha de color distinto,\ny si esta casilla no es una casilla segura (como las casillas de color), entonces esta\nsegunda ficha es devorada por la primera (es decir, que vuelve a la salida)."));
+    this->reglas.push_back(new Regla("10. El objetivo de este juego es que las fichas den una vuelta entera al tablero para\nllegar a 'casa', que se encuentra en la última casilla del camino en el tablero que\ndebe recorrer. Una vez que un jugador logre que sus cuatro fichas lleguen a la 'casa'\nse acaba el juego quedando este como ganador."));
+  }
 }
 
 /**
